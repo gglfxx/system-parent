f0e06731a5ee6da00f94fcbbf9fab9e1d409b62f
guiguoliang <guiguoliang@163.com>
2021-06-10 23:04:22 +0800

    1、修改redis配置

--- system-web/src/main/java/com/online/system/common/config/RedisConfig.java
+++ system-web/src/main/java/com/online/system/common/config/RedisConfig.java
@@ -1,12 +1,31 @@
 package com.online.system.common.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
-
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        // 为 key 设置序列化器
+        template.setKeySerializer(new StringRedisSerializer());
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        serializer.setObjectMapper(mapper);
+        // 为 value 设置序列化器
+        template.setValueSerializer(serializer);
+        return template;
+    }
 }
